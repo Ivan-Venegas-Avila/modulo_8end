3fab79527b74073ae03de80730a22135b6a4ae71
ivan-venegas-avila <ivan.venegas41@gmail.com>
2022-06-05 21:23:44 -0400

    modulo_8end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :tv_shows
   get '/tv_shows', to: 'tv_shows#index'
   get '/welcome', to: 'pages#welcome'
   post '/tv_shows', to: 'tv_shows#create'
   root 'pages#welcome'
+
+
 end
